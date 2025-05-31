f2ec8d03220a34658bbec7f36122e012fd557672
Tabrez Ahmed <tabrezahmed9534@gmail.com>
2025-05-31 15:53:25 +0530

    first commit

---
+++ Patterns/AllPattern.java
@@ -0,0 +1,57 @@
+public class AllPattern {
+    public static void main(String[] args) {
+        System.out.println("Print the right half pyramid\n*\n* *\n* * *\n* * * *\n* * * * *");
+
+
+
+        System.out.println("Print the reverse half pyramid\n* * * * *\n* * * *\n* * *\n* *\n*");
+
+
+
+        System.out.println("Print the Triangle\n    *\n   * *\n  * * *\n * * * *\n* * * * *");
+
+
+
+        System.out.println("Print the left half pyramid\n        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+
+
+
+        System.out.println("Print the Square Hollow Pattern\n*  *  *  *  *  *\n*              *\n*              *\n*              *\n*  *  *  *  *  *");
+
+
+
+        System.out.println("Print the Number triangular\n    1\n   2 2\n  3 3 3\n 4 4 4 4");
+
+
+        System.out.println("Print the number increasing pyramid \n1\n1 2\n1 2 3\n1 2 3 4");
+
+
+
+        System.out.println("Print the number increasing reverse pyramid\n1 2 3 4\n1 2 3\n1 2\n1");
+
+
+
+        System.out.println("print the number Changing pyramid \n1\n2 3\n4 5 6\n7 8 9 10");
+
+
+
+        System.out.println("Print zero-One Triangle\n1\n0 1\n1 0 1\n0 1 0 1");
+
+
+
+        System.out.println("Print palindrome Triangular \n       1\n     2 1 2\n   3 2 1 2 3\n 4 3 2 1 2 3 4");
+
+
+
+        System.out.println("Print the Rhombus Pattern\n * * * *\n  * * * *\n   * * * *\n    * * * *\n     * * * *");
+
+
+
+        System.out.println("Print the Diamond Pattern\n     *\n   *  *\n  *  *  *\n *  *  *  *\n  *  *  *\n   *  *\n     *");
+
+
+
+        System.out.println("Print the Butterfly Star Pattern\n*\n*\n*\n*\n");
+
+    }
+}
---
+++ Patterns/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!\n\nwelcome to Tabrez Ahmed\n");
+        System.out.print("Tabrez Ahmed\n");
+        System.out.println("One\nTwo\nThree");
+        System.out.println("Print the right pyramid");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *\n");
+
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*\n");
+
+        System.out.println("Print the right half pyramid");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+
+
+    }
+}
