29ee993a2edb50a6583f65422801de178be8713e
Tabrez Ahmed <tabrezahmad9534@gmail.com>
2025-06-01 18:28:31 +0530

    DataTypes

---
+++ DataTypes/CollectionsOfDataTypes.java
@@ -0,0 +1,43 @@
+package DataTypes;
+
+public class CollectionsOfDataTypes {
+    public static void main(String[] args) {
+        
+        int myInt =89;   // int store value from -2,147,483,648 to 2,147,483,647
+        System.out.println(myInt);
+
+        float myFloat;   // float store value from Range Approximately +- 3.40282347E=38F
+        myFloat = 80;
+        System.out.println(myFloat);
+        System.out.println(80);
+
+        String myString = "Welcome to Tabrez Ahmed";
+
+        System.out.println(myString);
+        System.out.println("Welcome to Tabrez Ahmed");
+
+
+        boolean myBoolean = true; // boolean store only 1bit  8bit=1 byte
+        System.out.println(myBoolean);
+        System.out.println(true);
+
+        byte myByte = 127; // byte store the value  from 0 to 127
+        System.out.println(myByte);//
+        System.out.println(127);
+
+        short ShortVariablesName = 32767; // short store value from -32768 to 32767;
+        System.out.println(ShortVariablesName);
+
+        long myLongValue = -546546519;  //  Range -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        System.out.println(myLongValue);
+
+        double myDoubleValue = 945646519;  // Range +- 1.797631348623157E+308
+        System.out.println(myDoubleValue);
+
+        char myChar = 'A';
+        System.out.println(myChar); // 0 to 65535
+
+
+
+    }
+}
