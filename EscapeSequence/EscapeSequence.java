e64e783795339d17a3b5031506608c92fe3c15ea
Tabrez Ahmed <tabrezahmad9534@gmail.com>
2025-06-03 02:06:53 +0530

    EscapeSequence

---
+++ EscapeSequence/EscapeSequence.java
@@ -0,0 +1,18 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+
+        System.out.println("java\tis\tfun");  // t for tab escape sequence to represent the four spaces between character and others
+
+        System.out.println("This\nis\nthe\nTabrez\nProgram"); //   /n for nextLine
+
+        System.out.println("Tabrez is \bhere");    // \b for backspace
+
+        System.out.println("Tabrez is \\'speaking'\\here");    // \' for single quote
+
+        System.out.println("i'm \"speaking\"");  //     \" for the double quote in between the Character
+
+        System.out.println("You \\need\\ to know");     //   \\ backslash represent the single back in between the quotation
+
+
+    }
+}
