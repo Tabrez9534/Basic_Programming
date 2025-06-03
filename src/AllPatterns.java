83338288b1b6807e98bcd33e4969e54d0e14daa4
Tabrez Ahmed <tabrezahmad9534@gmail.com>
2025-06-04 01:27:37 +0530

    Allpatterns

---
+++ src/AllPatterns.java
@@ -0,0 +1,83 @@
+public class AllPatterns {
+    public static void main(String[] args) {
+
+        System.out.println("* * * * * * * * *");
+        System.out.println("*               *");
+        System.out.println("*               *");
+        System.out.println("*               *");
+        System.out.println("*               *");
+        System.out.println("* * * * * * * * *");
+
+
+        System.out.println("* * * * * * * * *\n*               *\n*               *\n*               *\n*               *\n* * * * * * * * *\n");
+
+        System.out.println("    1");
+        System.out.println("   2 2");
+        System.out.println("  3 3 3");
+        System.out.println(" 4 4 4 4");
+
+        System.out.println("    1\n   2 2\n  3 3 3\n 4 4 4 4\n");
+
+
+        System.out.println("1");
+        System.out.println("1 2");
+        System.out.println("1 2 3");
+        System.out.println("1 2 3 4\n");
+        System.out.println("1\n1 2\n1 2 3\n1 2 3 4\n");
+
+
+        System.out.println("1 2 3 4\n1 2 3\n1 2\n1\n");
+        System.out.println("1 2 3 4");
+        System.out.println("1 2 3");
+        System.out.println("1 2");
+        System.out.println("1");
+
+
+//      Number Changing Pyramid
+
+        System.out.println("1");
+        System.out.println("2 3");
+        System.out.println("4 5 6");
+        System.out.println("7 8 9 10");
+        System.out.println("1\n2 3\n4 5 6\n7 8 9 10");
+
+
+//        Zero-One Triangle
+        System.out.println("1");
+        System.out.println("0 1");
+        System.out.println("1 0 1");
+        System.out.println("0 1 0 1\n");
+        System.out.println("1\n0 1\n1 0 1\n0 1 0 1");
+
+//        Palindrome Triangular
+        System.out.println("        1");
+        System.out.println("      2 1 2");
+        System.out.println("    3 2 1 2 3");
+        System.out.println("  4 3 2 1 2 3 4");
+        System.out.println("        1\n      2 1 2\n    3 2 1 2 3\n  4 3 2 1 2 3 4\n");
+
+
+//        Rhombus Pattern
+        System.out.println(" * * * *");
+        System.out.println("  * * * *");
+        System.out.println("   * * * *");
+        System.out.println("    * * * *");
+        System.out.println(" * * * *\n  * * * *\n   * * * *\n    * * * *\n");
+
+//        Diamond Pattern
+        System.out.println("        *");
+        System.out.println("      *   *");
+        System.out.println("    *   *   *");
+        System.out.println("  *   *   *   *");
+        System.out.println("    *   *   *");
+        System.out.println("      *   *");
+        System.out.println("        *\n");
+
+        System.out.println("       *\n     *   *\n   *   *   *\n *   *   *   *\n   *   *   *\n     *   *\n       *\n\n");
+
+        System.out.println("*\n   *\n*     *\n   *     *\n*     *\n   *\n*");
+
+
+
+    }
+}
